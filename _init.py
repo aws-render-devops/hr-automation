a9d051535010c4729e7fe9acd6c9add4a8302f78
antonapalanyuk <anton@lubbu.com>
2023-07-12 21:59:03 +0400

    Add Flask to trigger render

--- _init.py
+++ _init.py
@@ -1,6 +1,11 @@
-
 import asyncio
+from app import app
+from flask import Flask
 
-if __name__ == '__main__':
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(host=app.config.get("HOST", "0.0.0.0"), port=app.config.get("PORT", 5000))
     from app.services.tasks import initial_task
+
     asyncio.run(initial_task())
