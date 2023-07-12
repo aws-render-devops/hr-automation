e24556aee1341cd7358acc0905b2a5ba77854697
antonapalanyuk <anton@lubbu.com>
2023-07-12 23:35:32 +0400

    Quickfix

--- _init.py
+++ _init.py
@@ -1,11 +1,6 @@
 import asyncio
-from app import app
-from flask import Flask
-
-app = Flask(__name__)
 
 if __name__ == "__main__":
-    app.run(host=app.config.get("HOST", "0.0.0.0"), port=app.config.get("PORT", 5000))
     from app.services.tasks import initial_task
 
     asyncio.run(initial_task())
--- main.py
+++ main.py
@@ -17,27 +17,26 @@ async def start_background_tasks(app):
 
 async def cleanup_background_tasks(app):
     # Stop the cron job when the application is shutting down
-    app['my_cron_job'].stop()
+    app["my_cron_job"].stop()
 
 
 async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
+    name = request.match_info.get("name", "Anonymous")
     text = "Hello, " + name
     return web.Response(text=text)
 
 
 async def health(request):
-    return web.Response(text='OK')
+    return web.Response(text="OK")
 
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
-app.add_routes([web.get('/-/health', health)])
+app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+app.add_routes([web.get("/-/health", health)])
 
 app.on_startup.append(start_background_tasks)
 app.on_shutdown.append(cleanup_background_tasks)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     web.run_app(app)
